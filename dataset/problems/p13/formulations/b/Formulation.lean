87fe1c5cad137e6bf6746ce57a494502bd97b2f5
Connor Lawless <connorlawless@Connors-Laptop.local>
2026-04-29 17:50:07 -0400

    [p13 issue] changed formulations to correspond to same problem

--- dataset/problems/p13/formulations/b/Formulation.lean
+++ dataset/problems/p13/formulations/b/Formulation.lean
@@ -11,10 +11,10 @@ namespace P13.b
 structure Params where
   nP : ℕ  -- number of flights
   nA : ℕ  -- number of locations
-  nT : ℕ  -- number of event times
+  nT : ℕ  -- number of time periods
   tau : Fin nA → Fin nA → ℤ  -- transition time from location a to a'
-  r : Fin nA → Fin nT → ℝ  -- reward for being at location a at event time t
-  cap : Fin nA → Fin nT → ℤ  -- capacity of location a at event time t
+  r : Fin nA → Fin nT → ℝ  -- reward for being at location a at time t
+  cap : Fin nA → Fin nT → ℤ  -- capacity of location a at time t
   -- Implicit Assumptions
   hnP : NeZero nP
   hnA : NeZero nA
@@ -23,25 +23,27 @@ structure Params where
   hcap_nn : ∀ a t, 0 ≤ cap a t
 
 structure Vars where
-  x : ℕ → ℕ → ℕ → ℕ → ℤ  -- 1 if flight p departs location a to a' at event time t
+  n : ℕ → ℕ → ℤ  -- number of flights at location a at time t
+  f : ℕ → ℕ → ℕ → ℤ  -- number of flights departing from location a to a' at time t
 
 structure Feasible (p : Params) (v : Vars) : Prop where
-  -- Each flight must make at least one trip
-  htrip : ∀ pl : Fin p.nP,
-    1 ≤ ∑ a : Fin p.nA, ∑ a' : Fin p.nA, ∑ t : Fin p.nT, v.x pl a a' t
-  -- Respect location capacity at each event time
-  hcap : ∀ (a : Fin p.nA) (t : Fin p.nT),
-    (∑ pl : Fin p.nP, ∑ a' : Fin p.nA, (v.x pl a a' t : ℝ)) ≤ (p.cap a t : ℝ)
-  -- x_{p,a,a',t} ∈ {0,1}
-  hx_bin : ∀ (pl : Fin p.nP) (a a' : Fin p.nA) (t : Fin p.nT),
-    v.x pl a a' t = 0 ∨ v.x pl a a' t = 1
+  -- All flights are accounted for at every time period
+  hcount : ∀ t : Fin p.nT, ∑ a : Fin p.nA, v.n a t = (p.nP : ℤ)
+  -- Number of flights at each location does not exceed capacity
+  hcap : ∀ (a : Fin p.nA) (t : Fin p.nT), (v.n a t : ℝ) ≤ (p.cap a t : ℝ)
+  -- Flow conservation with travel time (for t > 0)
+  hflow : ∀ (a : Fin p.nA) (t : Fin p.nT), 0 < t.val →
+    v.n a t =
+      v.n a (t.val - 1) +
+      ∑ a' : Fin p.nA, (univ.filter (fun t' : Fin p.nT => t'.val + p.tau a' a = t.val)).sum
+              (fun t' => v.f a' a t') -
+      ∑ a' : Fin p.nA, v.f a a' t
+  hn_nn : ∀ (a : Fin p.nA) (t : Fin p.nT), 0 ≤ v.n a t
+  hf_nn : ∀ (a a' : Fin p.nA) (t : Fin p.nT), 0 ≤ v.f a a' t
 
--- Maximize total reward (reward accrues at arrival times)
+-- Maximize total reward for all flights across all locations and time periods
 def obj (p : Params) (v : Vars) : ℝ :=
-  ∑ pl : Fin p.nP, ∑ a : Fin p.nA, ∑ t : Fin p.nT, p.r a t *
-    ∑ a' : Fin p.nA,
-      (univ.filter (fun t' : Fin p.nT => t'.val + p.tau a a' = t.val)).sum
-        (fun t' => (v.x pl a a' t' : ℝ))
+  ∑ a : Fin p.nA, ∑ t : Fin p.nT, p.r a t * (v.n a t : ℝ)
 
 def formulation : MILPFormulation where
   Params   := Params
